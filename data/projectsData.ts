d29a07dd0ff9856c1e2d6747e04ab55af160a2f9
Kenny <22737987+KennyLMQ@users.noreply.github.com>
2022-11-09 12:11:29 +0800

    feat: update project data

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,20 +1,12 @@
 const projectsData = [
-  // {
-  //   title: 'A Search Engine',
-  //   description: `What if you could look up any information in the world? Webpages, images, videos
-  //   and more. Google has many features to help you find exactly what you're looking
-  //   for.`,
-  //   imgSrc: '/static/images/google.png',
-  //   href: 'https://www.google.com',
-  // },
-  // {
-  //   title: 'The Time Machine',
-  //   description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-  //   to the desired date and press "Go". No more worrying about lost keys or
-  //   forgotten headphones with this simple yet affordable solution.`,
-  //   imgSrc: '/static/images/time-machine.jpg',
-  //   href: '/blog/the-time-machine',
-  // },
+  {
+    title: 'Football xG',
+    description: `Expected Goals (xG) is a stastical approach to predict a 
+    team's chance of scoring goals. This project consolidates these statistics
+    for the world's most popular football league - the English Premier League.`,
+    imgSrc: '/static/images/football-xg.png',
+    href: 'https://football.kennylmq.com',
+  },
 ]
 
 export default projectsData
