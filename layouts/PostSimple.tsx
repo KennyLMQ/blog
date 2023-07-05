28293e9da5f6f91174db3223e8b834e21c6d1141
KennyLMQ <22737987+KennyLMQ@users.noreply.github.com>
2023-07-05 23:53:30 +0800

    chore: run linting

--- layouts/PostSimple.tsx
+++ layouts/PostSimple.tsx
@@ -47,7 +47,7 @@ export default function PostLayout({ content, next, prev, children }: LayoutProp
             style={{ gridTemplateRows: 'auto 1fr' }}
           >
             <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
-              <div className="prose max-w-none pt-10 pb-8 dark:prose-dark">{children}</div>
+              <div className="prose max-w-none pb-8 pt-10 dark:prose-dark">{children}</div>
             </div>
             {/* {siteMetadata.comments && (
               <div className="pt-6 pb-6 text-center text-gray-700 dark:text-gray-300" id="comment">
